2a7763be8088b4fd058b5d80210a61e7f1db4422
Kaic Melo Santos <kakakaic@yahoo.com.br>
2022-04-30 15:25:06 -0300

    changing js to ts

--- clockin-backoffice-v2/cypress/integration/WEB/1-Login/login.ts
+++ clockin-backoffice-v2/cypress/integration/WEB/1-Login/login.ts
@@ -4,18 +4,28 @@ describe("Logging in to the backoffice", () => {
   });
 
   it("Verificar login invalido", () => {
-    cy.login(Cypress.env('emailFake'), Cypress.env('password'));
+    cy.get("#email").type(Cypress.env('emailFake'));
+    cy.get("#password").type(Cypress.env('password'));
+
+    cy.get('button[type="submit"]').click();
     cy.contains('.gate-error', 'Incorrect login was used.').should('be.visible');
   });
 
   it("Verificar senha invalida", () => {
-    cy.login(Cypress.env('email'), '123456789');
+    cy.get('#email').type(Cypress.env('email'));
+    cy.get("#password").type('123456789');
+
+    cy.get('button[type="submit"]').click();
+
     cy.contains('.gate-error', 'Incorrect password was used').should('be.visible');
   });
 
   it("Verificar Environment invalido", () => {
     cy.get("#tenant").type('clockinBeta');
-    cy.loginNoClick(Cypress.env('email'), Cypress.env('password'));
+
+    cy.get("#email").type(Cypress.env('email'));
+    cy.get("#password").type(Cypress.env('password'));
+
     cy.contains('li', 'Environment not found').should('be.visible');
   });
 
@@ -50,13 +60,18 @@ describe("Logging in to the backoffice", () => {
     cy.contains('p', ' An email with instructions to reset your password has been sent to ').should('be.visible');
   });
 
-  it("Verificar esqueci minha senha com email valido depois fazer login", () => {
+  it.only("Verificar esqueci minha senha com email valido depois fazer login", () => {
     cy.get('.gate-nav').click();
     cy.get('#email').type(Cypress.env('email'));
     cy.get('.gate-btn').click();
     cy.wait(2000);
     cy.get('.gate-btn').click();
-    cy.login(Cypress.env('email'), Cypress.env('password'));
+    
+    
+    cy.get("#email").type(Cypress.env('email'));
+    cy.get("#password").type(Cypress.env('password'));
+
+    cy.get('button[type="submit"]').click();
     cy.contains('cw-card-title', 'Select an environment').should('be.visible');
   });
 });
